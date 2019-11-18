2beee3da9e0fece8b3566f737b7f869403f80d7f
Benedict <asadu.benedict@gmail.com>
2019-11-18 20:55:04 +0100

    Dashboard Route Implemented with necessary things

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -1,12 +1,35 @@
 from flask import render_template, session, redirect
-from werkzeug.security import check_password_hash, generate_password_hash
-from controllers.error import error
-from controllers.auth import logged_in
 
 def dashboard_handler(database):
 
-    user = database.execute("SELECT * FROM users WHERE username=:username", username=session.get("username"))
+    #Handles Authentication of User
+    if not session.get("username"):
+        return redirect("/login")
 
-    if user[0]["user_type"] == "user":
-        
-    pass
+    #Retrieves User Information To Enable Processing
+    user = database.execute("SELECT (user_type, user_view) FROM users WHERE username=:username", username=session.get("username"))
+    session["user_type"] = user[0]["user_type"]
+    user_type = user[0]["user_type"]
+    user_view = user[0]["user_view"]
+
+    #Renders Admin DashBoard
+    if user_type == "admin":
+        userdetail = database.execute("SELECT (name, username, email, phone_number, address, user_image, balance,) FROM users WHERE username=:username",
+                                                                        username=session.get("username"))
+        return render_template("user_management.html", user=userdetail[0])
+
+    #Renders Vendor DashBoard
+    if user_type == "vendor" and user_view == "vendor":
+        userdetail = database.execute("SELECT (username, email, user_image, balance, business_name, business_address, business_number) FROM users WHERE username=:username", 
+                                                                        username=session.get("username"))
+        return render_template("vendor_dashboard.html", user=userdetail[0])
+
+    #Renders Buyer DashBoard
+    if user_type == "user" or user_view == "user":
+        userdetail = database.execute("SELECT (name, username, email, phone_number,	address, user_image, balance, status, user_view) FROM users WHERE username=:username",
+                                                                        username=session.get("username"))
+        return render_template("dashboard.html", user=userdetail[0])
+    
+    session.clear()
+    return redirect("/login")
+
