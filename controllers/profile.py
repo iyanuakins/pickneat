4c9b9e1341d4c613e322a53e9809dd0e3ab4c93a
Benedict <asadu.benedict@gmail.com>
2019-11-19 13:12:44 +0100

    Renders Appropriate Info for different User

--- controllers/profile.py
+++ controllers/profile.py
@@ -1,68 +1,57 @@
+import os
 from flask import render_template, session, redirect
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 def profile_handler(request, database):
     #Handles Authentication of User
     if not session.get("username"):
         return redirect("/login")
 
-    user_type = session.get("user_type")
-    user_view = session.get("user_view")
+    #Retrieves User Information from Database
+    user = database.execute("SELECT * FROM users WHERE username=:username", username=session.get("username"))
 
     #Handles GET request to Page
     if request.method == "GET":
-        return get_profile(user_type, user_view, database)
+        return render_template("profile.html", user=user[0])
 
-    if user_type == "admin":
-        return update_admin_info(request, database)
-    elif user_type == "vendor" and user_view == "vendor":
-        return update_vendor_info(request, database)
-    elif user_type == "user" or user_view == "user":
-        return update_user_info(request, database)
+    #Dummy for Image Url
+    user_image = ""
+    extension = ""
 
+    #Create Filename for File
+    if request.files["user_image"]:
+        image = request.files["user_image"]
+        image_name = secure_filename(image.filename)
+        user_image = image.filename
+        extension = user_image.rsplit(".", 1)[1]
+        user_image = datetime.now().strftime("%m%d%Y%H%M%S")+"."+extension
 
-def get_profile(user_type, user_view, database):
+    #Checks for Image ELigibility for possible change to default
+    if not "." in user_image:
+        user_image = user[0]["user_image"]
+    elif not extension:
+        if not extension.upper() in ["JPEG", "JPG", "PNG", "GIF"]:
+            user_image = user[0]["user_image"]
+    else:
+        #Removes previous User Image
+       os.remove(os.path.join("static/images", user[0]["user_image"]))
 
-    #Verifying Current User
-    if user_type == "admin":
-        userdetail = database.execute("SELECT (name, username, email, phone_number, address, user_image, balance,) FROM users WHERE username=:username",
-                                                                        username=session.get("username"))
-    elif user_type == "vendor" and user_view == "vendor":
-        userdetail = database.execute("SELECT (username, email, user_image, balance, business_name, business_address, business_number) FROM users WHERE username=:username", 
-                                                                        username=session.get("username"))
-    elif user_type == "user" or user_view == "user":
-        userdetail = database.execute("SELECT (name, username, email, phone_number,	address, user_image, balance, status) FROM users WHERE username=:username",
-                                                                        username=session.get("username"))
+    #Updates User Information to Database
+    database.execute("UPDATE users SET (name, email, phone_number, address, user_image, business_name, business_address, business_number)=(:name, :email, :phone_number, :address, :user_image, :business_name, :business_address, :business_number) WHERE username=:username",
+                            name = request.form.get("name") if request.form.get("name") else user[0]["name"],
+                            email = request.form.get("email") if request.form.get("email") else user[0]["email"],
+                            phone_number = request.form.get("phone_number") if request.form.get("phone_number") else user[0]["phone_number"],
+                            address = request.form.get("address") if request.form.get("address") else user[0]["address"],
+                            user_image = user_image,
+                            business_name = request.form.get("business_name") if request.form.get("business_name") else user[0]["business_name"],
+                            business_address = request.form.get("business_address") if request.form.get("business_address") else user[0]["business_address"],
+                            business_number = request.form.get("business_number") if request.form.get("business_number") else user[0]["business_number"],
+                            username=session.get("username"))
 
-    #Renders Appropriate Profile Page
-    return render_template("profile.html", user=userdetail[0])
+    #Stores New Image in Project
+    if request.files["user_image"]:
+        image.save(os.path.join("static/images", user_image))
     
-
-def update_admin_info(request, database):
-    database.execute("UPDATE users SET (name,username,email,phone_number,address,user_image)=(:name,:email,:phone,:address,:image) users WHERE username=:username",
-                                                                        name = request.form.get("name"),
-                                                                        email = request.form.get("email"),
-                                                                        phone = request.form.get("phone"),
-                                                                        address = request.form.get("address"),
-                                                                        image = request.form.get("image"),
-                                                                        username=session.get("username"))
-    return redirect("/profile")
-
-def update_vendor_info(request, database):
-    database.execute("UPDATE users SET (business_name, email, business_number,business_address, image_url)=(:name,:email,:phone,:address,:image) users WHERE username=:username",
-                                                                        name = request.form.get("business_name"),
-                                                                        email = request.form.get("business_email"),
-                                                                        phone = request.form.get("business_number"),
-                                                                        address = request.form.get("business_address"),
-                                                                        image = request.form.get("image_url"),
-                                                                        username=session.get("username"))
-    return redirect("/profile")
-
-def update_user_info(request, database):
-    database.execute("UPDATE users SET (name,username,email,phone_number,address,user_image)=(:name,:email,:phone,:address,:image) users WHERE username=:username",
-                                                                        name = request.form.get("name"),
-                                                                        email = request.form.get("email"),
-                                                                        phone = request.form.get("phone"),
-                                                                        address = request.form.get("address"),
-                                                                        image = request.form.get("image"),
-                                                                        username=session.get("username"))
+    #Refreshes Page
     return redirect("/profile")
