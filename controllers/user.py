03ae24c200d52a8ad5946c6bf26bb3f829933100
Oluwasayo <oluwasayoakinkunmi@gmail.com>
2019-11-20 12:37:07 +0100

    updated templates and stylesheets

--- controllers/user.py
+++ controllers/user.py
@@ -59,7 +59,10 @@ def profile_handler(request, database):
     #Create Filename for File
     if request.files["user_image"]:
         image = request.files["user_image"]
-        image_name = secure_filename(image.filename)
+        imag
+        
+        
+        e_name = secure_filename(image.filename)
         user_image = image.filename
         extension = user_image.rsplit(".", 1)[1]
         user_image = "static/images/"+datetime.now().strftime("%m%d%Y%H%M%S")+"."+extension
